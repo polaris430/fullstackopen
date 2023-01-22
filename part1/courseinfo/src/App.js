f06da4ca7dd03fdde6532aa30444db0f5a799c59
polaris430 <sapras90@icloud.com>
2023-01-22 15:45:48 +0530

    part1 courseinfo solution

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -7,27 +7,28 @@ const Header = (props) => {
   );
 };
 
-const Part = (props) => {
+// const Part = (props) => {
+//   console.log(props);
+//   return (
+//     <div>
+//       {props.part} {props.exercise}
+//     </div>
+//   );
+// };
+
+const Content = (props) => {
   console.log(props);
   return (
     <div>
-      {props.part} {props.exercise}
-    </div>
-  );
-};
-
-const Content = () => {
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
-  return (
-    <div>
-      <Part part={part1} exercise={exercises1} />
-      <Part part={part2} exercise={exercises2} />
-      <Part part={part3} exercise={exercises3} />
+      <p>
+        {props.parts[0].name} {props.parts[0].exercises}
+      </p>
+      <p>
+        {props.parts[1].name} {props.parts[1].exercises}
+      </p>
+      <p>
+        {props.parts[2].name} {props.parts[2].exercises}
+      </p>
     </div>
   );
 };
@@ -36,22 +37,40 @@ const Tail = (props) => {
   console.log(props);
   return (
     <div>
-      <p>Number of exercises {props.total}</p>
+      <p>
+        Number of exercise{" "}
+        {props.parts[0].exercises +
+          props.parts[1].exercises +
+          props.parts[2].exercises}
+      </p>
     </div>
   );
 };
 
 const App = () => {
-  const course = "Half Stack application development";
-  const exercises1 = 10;
-  const exercises2 = 7;
-  const exercises3 = 14;
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
-      <Header course={course} />
-      <Content />
-      <Tail total={exercises1 + exercises2 + exercises3} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Tail parts={course.parts} />
     </div>
   );
 };
