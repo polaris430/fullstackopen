195ad5b7273be5fcda1611b78f8f23d0d9565503
polaris430 <sapras90@icloud.com>
2023-01-24 02:05:39 +0530

    part1 solution added

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,5 +1,12 @@
 import { useState } from "react";
 
+//StatisticsLine component
+const StatisticLine = (props) => (
+  <div>
+    {props.text} {props.value}
+  </div>
+);
+
 // Statistics component showing feedback stats
 const Statistics = (props) => {
   if (props.all.length === 0) {
@@ -11,18 +18,44 @@ const Statistics = (props) => {
   }
   return (
     <div>
-      <div>
-        <h1>statistics</h1>
-      </div>
-      <div>good {props.good}</div>
-      <div>neutral {props.neutral}</div>
-      <div>bad {props.bad}</div>
-      <div>
-        all{" "}
-        {props.all.reduce(function (x, y) {
-          return x + y;
-        }, 0)}
-      </div>
+      <h1>statistics</h1>
+      <table>
+        <tr>
+          <td>
+            <StatisticLine text="good" value={props.good} />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <StatisticLine text="neutral" value={props.neutral} />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <StatisticLine text="bad" value={props.bad} />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <StatisticLine
+              text="all"
+              value={props.all.reduce(function (x, y) {
+                return x + y;
+              }, 0)}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <StatisticLine text="avg" value={props.avg} />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <StatisticLine text="positive%" value={props.positive} />
+          </td>
+        </tr>
+      </table>
     </div>
   );
 };
@@ -38,8 +71,6 @@ const Button = (props) => {
   );
 };
 
-//StatisticsLine component
-
 //App component
 const App = () => {
   // save clicks of each button to its own state
@@ -47,6 +78,8 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
   const [all, setAll] = useState([]);
+  const [avg, setAvg] = useState(0);
+  const [positive, setPositive] = useState(0);
 
   return (
     <div>
@@ -58,20 +91,63 @@ const App = () => {
         handleGoodClick={() => {
           setGood(good + 1);
           setAll(all.concat(1));
+          setAvg(
+            all.reduce(function (x, y) {
+              return x + y;
+            }, 0) / 3
+          );
+          setPositive(
+            (good /
+              all.reduce(function (x, y) {
+                return x + y;
+              }, 0)) *
+              100
+          );
         }}
         text1="good"
         handleNeutralClick={() => {
           setNeutral(neutral + 1);
           setAll(all.concat(1));
+          setAvg(
+            all.reduce(function (x, y) {
+              return x + y;
+            }, 0) / 3
+          );
+          setPositive(
+            (good /
+              all.reduce(function (x, y) {
+                return x + y;
+              }, 0)) *
+              100
+          );
         }}
         text2="neutral"
         handleBadClick={() => {
           setBad(bad + 1);
           setAll(all.concat(1));
+          setAvg(
+            all.reduce(function (x, y) {
+              return x + y;
+            }, 0) / 3
+          );
+          setPositive(
+            (good /
+              all.reduce(function (x, y) {
+                return x + y;
+              }, 0)) *
+              100
+          );
         }}
         text3="bad"
       />
-      <Statistics good={good} neutral={neutral} bad={bad} all={all} />
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        all={all}
+        avg={avg}
+        positive={positive}
+      />
     </div>
   );
 };
